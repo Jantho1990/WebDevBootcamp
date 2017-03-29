0027b5f858bd81442ad305e24894d5a3400e5a06
Jantho1990 <jantho1990@gmail.com>
2017-03-28 20:36:49 -0500

    16

---
+++ 16/exercise1.js
@@ -0,0 +1,5 @@
+$('div').css('background', 'purple');
+$('.highlight').css('width', '200px');
+$('#third').css('border', '2px solid orange');
+// $('div').eq(0).css('color', 'pink');
+$('div:first-of-type').eq(0).css('color', 'pink');
